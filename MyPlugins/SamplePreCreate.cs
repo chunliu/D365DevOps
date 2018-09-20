8225f042a3352a034db603137f4762f34bab13cf
Chun Liu <chunliu@microsoft.com>
2018-09-20 21:44:24 +1000

    Add plugin project
    Related Work Items: #485

---
+++ MyPlugins/SamplePreCreate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlugins
+{
+    [CrmPluginRegistration(MessageNameEnum.Create, "new_sample", StageEnum.PreOperation, ExecutionModeEnum.Synchronous,
+        "", "new_sample PreCreate", 1, IsolationModeEnum.Sandbox)]
+    public class SamplePreCreate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Do nothing
+        }
+    }
+}
